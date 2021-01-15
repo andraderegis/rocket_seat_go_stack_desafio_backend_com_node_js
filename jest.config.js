9f2e90fac0a1316958783acf063ef6e69561ae8f
andraderegis <reginaldo.andrade@outlook.com>
2021-01-15 15:06:52 -0300

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+
+module.exports = {
+    rootDir: root,
+    displayName: 'root-tests',
+    testMatch: ['<rootDir>/src/**/*.test.js'],
+    testEnvironment: 'node'
+};
