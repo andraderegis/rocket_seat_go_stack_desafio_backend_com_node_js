5f6d6a37eb3c34fa87347f6b901c044bc00675bf
andraderegis <reginaldo.andrade@outlook.com>
2021-01-15 11:25:23 -0300

    Development and tests for update repositories

--- src/__tests__/respositories-route.test.js
+++ src/__tests__/respositories-route.test.js
@@ -1,34 +1,104 @@
 const request = require('supertest');
 const app = require('../app');
-const { isUuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 describe('Tests for Repositories Routes', () => {
-
     it('Should be able to create a new repository', async () => {
+        const respositoryDataToCreate = {
+            title: 'Rocket Seat Go Stack Desafio Back-end com NodeJS',
+            url:
+                'https://github.com/andraderegis/rocket_seat_go_stack_desafio_backend_com_node_js',
+            techs: ['Node.js', 'ExpressJS', 'Jest']
+        };
+
         const response = await request(app)
             .post('/repositories')
-            .send({
-                title: "Rockect Seat Go Stack Desafio Back-end com NodeJS",
-                url: "https://github.com/andraderegis/rocket_seat_go_stack_desafio_backend_com_node_js",
-                techs: ["Node.js", "ExpressJS", "Jest"]
-            })
+            .send(respositoryDataToCreate)
             .expect(201);
 
         const body = response.body;
-
-        expect(body).toBeDefined();
-
         const { id } = body;
 
+        expect(body).toBeDefined();
         expect(isUuid(id)).toBeTruthy();
-
+        expect(body).toMatchObject(respositoryDataToCreate);
     });
     it('Should be able to list the repositories', async () => {
-        const response = await request(app)
-            .get('/repositories')
-            .expect(200);
+        const response = await request(app).get('/repositories').expect(200);
 
         const body = response.body;
 
-        expect(body.length).toBeTruthy();    
+        expect(body.length).toBeTruthy();
+    });
+
+    it('Should be able to update repository', async () => {
+        const respositoryDataToCreate = {
+            title: 'Go Stack Desafio Back-end com NodeJS',
+            url:
+                'https://github.com/andraderegis/rocket_seat_go_stack_desafio_backend_com_node_js',
+            techs: ['Node.js', 'ExpressJS', 'Jest']
+        };
+
+        const createResponse = await request(app)
+            .post('/repositories')
+            .send(respositoryDataToCreate);
+
+        const { id } = createResponse.body;
+
+        const repositoryDataToUpdate = {
+            title: 'Golang Desafio K8S HPA',
+            url:
+                'https://github.com/andraderegis/full-cycle-code-education-golang-desafio-k8s-hpa',
+            techs: ['Golang', 'Docker', 'Kubernets', 'Google Cloud Plataform']
+        };
+
+        const updateResponse = await request(app)
+            .put(`/repositories/${id}`)
+            .send(repositoryDataToUpdate)
+            .expect(200);
+
+        const body = updateResponse.body;
+
+        expect(body).toBeDefined();
+        expect(body).toMatchObject(repositoryDataToUpdate);
+    });
+
+    it('Should not be able to update a repository that does not exist', async () => {
+        const response = await request(app)
+            .put(`/repositories/${uuid()}`)
+            .send({})
+            .expect(400);
+
+        expect(response.body).toMatchObject({
+            error: 'Not found repository.'
+        });
+    });
+
+    it('should not be able to update repository likes manually', async () => {
+        const expectedLikes = 0;
+
+        const respositoryDataToCreate = {
+            title: 'Desafio Back-end com NodeJS',
+            url:
+                'https://github.com/andraderegis/rocket_seat_go_stack_desafio_backend_com_node_js',
+            techs: ['Node.js', 'ExpressJS', 'Jest', 'Docker']
+        };
+
+        const createResponse = await request(app)
+            .post('/repositories')
+            .send(respositoryDataToCreate);
+
+        expect(createResponse.body.likes).toEqual(expectedLikes);
+
+        const respositoryDataToUpdate = {
+            techs: ['Node.js', 'ExpressJS', 'Jest']
+        };
+
+        const updateResponse = await request(app)
+            .put(`/repositories/${createResponse.body.id}`)
+            .send(respositoryDataToUpdate)
+            .expect(200);
+
+        expect(updateResponse.body.likes).toEqual(expectedLikes);
+        expect(updateResponse.body).toMatchObject(respositoryDataToUpdate);
     });
 });
--- src/routes/respositories-route.js
+++ src/routes/respositories-route.js
@@ -1,6 +1,7 @@
 const { uuid } = require('uuidv4');
 
 const respositories = [];
+const _findRepositoriesIndex = Symbol('findRepositoriesIndex');
 
 class RepositoriesRoute {
     constructor(router) {
@@ -21,7 +22,7 @@ class RepositoriesRoute {
                 url,
                 techs,
                 likes: 0
-            }
+            };
 
             respositories.push(repository);
 
@@ -29,18 +30,39 @@ class RepositoriesRoute {
         });
 
         this._router.put('/:id', (req, res) => {
+            const { id } = req.params;
 
+            const repositoryIndex = this[_findRepositoriesIndex](id);
+
+            if (repositoryIndex < 0) {
+                return res.status(400).json({
+                    error: 'Not found repository.'
+                });
+            }
+
+            const { title, url, techs } = req.body;
+
+            const respository = {
+                ...respositories[repositoryIndex],
+                title,
+                url,
+                techs
+            };
+
+            respositories[repositoryIndex] = respository;
+
+            return res.status(200).json(respository);
         });
 
-        this._router.delete('/:id', (req, res) => {
+        this._router.delete('/:id', (req, res) => {});
 
-        });
-        
-        this._router.post('/:id/like', (req, res) => {
-
-        });
+        this._router.post('/:id/like', (req, res) => {});
 
         return this._router;
+    }
+
+    [_findRepositoriesIndex](id) {
+        return respositories.findIndex((repository) => repository.id === id);
     }
 }
 
